499ed965636932c4b59bf1b045b32353a45a5e49
root <root@H08.hbtn.uy>
2024-11-24 15:22:00 -0300

    Arrays - Javascript

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,6 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-
       const vargrade = newGrades.find((grade) => grade.studentId === student.id);
 
       return {
