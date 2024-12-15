8ff9441240409f6e4f72883e7683dbc576caec6f
root <root@H08.hbtn.uy>
2024-12-15 18:45:53 -0300

    Checker fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,20 +1,20 @@
-    #!/usr/bin/env python3
+#!/usr/bin/env python3
+"""
+python
+"""
+
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
     """
-    python
+    Generador asincrono entre 0 and 10.
+
+    Yields:
+        float: Random float
     """
-
-    import random
-    import asyncio
-    from typing import Generator
-
-
-    async def async_generator() -> Generator[float, None, None]:
-        """
-        Generador asincrono entre 0 and 10.
-
-        Yields:
-            float: Random float
-        """
-        for _ in range(10):
-            await asyncio.sleep(1)  # Simulate asynchronous operation
-            yield random.uniform(0, 10)
+    for _ in range(10):
+        await asyncio.sleep(1)  # Simulate asynchronous operation
+        yield random.uniform(0, 10)
