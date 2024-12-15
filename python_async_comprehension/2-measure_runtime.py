c35c09e0036a1d729754ae65416bae806f2bd8d2
root <root@H08.hbtn.uy>
2024-12-15 18:50:27 -0300

    Checker fix

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -12,7 +12,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """
-    Measure the total runtime of running async_comprehension four times in parallel.
 
     Returns:
         float: Total runtime in seconds.
