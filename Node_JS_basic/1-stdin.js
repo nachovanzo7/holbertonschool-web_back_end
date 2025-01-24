153ea2dd974ec3499715e612d417bf8d2867456b
Nacho Vanzo <9426@holbertonstudents.com>
2025-01-24 15:19:22 -0300

    Fix ex-1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,8 +2,11 @@ process.stdout.write("Welcome to Holberton School, what is your name?");
 
 process.stdin.on('data', (data) => {
     const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
+    const message = `Your name is: ${name}\n`
 
+    process.stdout.write('\n' + message);
+
+    
     process.stdout.write("This important software is now closing\n");
     process.exit();
 });
